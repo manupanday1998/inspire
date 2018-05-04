2b96054c0bcff84d2cc70b6b94505cb473c7b115
Pranav pandey <manupanday1998@gmail.com>
2018-05-04 18:00:37 +0530

    2nd commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/*var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
+	typeSpeed : 50,
+	backSpeed : 25,
+	showCursor: false,
+	loop: true,
+});*/
+
+// slideshow code
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
